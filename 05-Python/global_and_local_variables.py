b1d1a4f099b390ac5d7c96b395db3c5a9951038d
unknown <Akbar Zeynalli>
2025-05-24 22:49:10 +0400

    first add 05-Python

---
+++ 05-Python/global_and_local_variables.py
@@ -0,0 +1,14 @@
+# text = '40000$ olacaq'
+
+# def my_function():
+#      global text
+#      text = 'data arcitect';
+#      print('Mənim Adobe Stock da aylıq gəlirim' + text)
+
+# my_function()
+# print(text)
+
+def salam_vetendas(ad):
+    print('Salam'+ad+'!')
+
+salam_vetendas = 'Əkbər'
